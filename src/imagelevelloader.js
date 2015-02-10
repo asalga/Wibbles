8bf89703a9d5399ef994524225e4ad067513bd46
Andor Salga <andorsalga@gmail.com>
2015-02-09 23:24:16 -0500

    added imageloader.

---
+++ src/imagelevelloader.js
@@ -0,0 +1,81 @@
+/*
+	Loads a level based on a jpg
+*/
+var ImageLevelLoader = (function(){
+	
+	'use strict';
+	var instance;
+
+	var ImageLevelLoader = function(){
+		
+		var isWall = function(r,g,b){
+			return (r === 255 && g === 96 && b === 96);
+		};
+
+
+		this.load = function(options){
+
+			var img = new Image();
+			img.src = options.levelPath;
+
+			img.onload = function(){
+				var canvas = document.createElement('canvas');
+
+				canvas.width = this.width;
+				canvas.height = this.height;
+				
+				var numCols = this.width;
+				var numRows = this.height;
+
+				var context = canvas.getContext('2d');
+				context.drawImage(this, 0, 0);
+
+				//
+				var returnedData = new Array(this.height);
+
+				for(var i = 0; i < this.height; i++){
+					returnedData[i] = new Array(this.width);
+				}
+				
+				// order is RGBA
+				// clampedUint8  [255, 64, 64, 255,      255, 64, ......]
+				var rawDataFlat = context.getImageData(0, 0, this.width, this.height).data;
+
+				var y = 0;
+				var x = 0;
+				for(var colIdx = 0; colIdx < rawDataFlat.length; colIdx += 4, x++){
+
+					// if we reached the edge of the 'image width'
+					if(colIdx !== 0 && colIdx % (numCols*4) === 0){
+						y++;
+						x = 0;
+					}
+
+					var r = rawDataFlat[colIdx + 0];
+					var g = rawDataFlat[colIdx + 1];
+					var b = rawDataFlat[colIdx + 2];
+					// alpha is used
+
+					if(isWall(r,g,b)){
+						returnedData[y][x] = 1;
+					}
+					else{
+						returnedData[y][x] = 0;
+					}
+				}
+				
+				options.done(returnedData);
+			};
+		};
+	};
+	
+	return {
+		getInstance: function(){
+			if(instance === WIBBLES.undef){
+				instance = new ImageLevelLoader();
+			}
+			return instance;
+		}
+	};
+
+})();
--- src/wibbles.js
+++ src/wibbles.js
@@ -1,12 +1,12 @@
 /*
     Snake
 */
-"use strict";
 
 var WIBBLES = {};
 
-var BLOCK_SIZE = 8;
+WIBBLES.undef/*left undefined*/;
 
+var BLOCK_SIZE = 8;
 
 /*
 
@@ -14,8 +14,8 @@ var BLOCK_SIZE = 8;
 function start() {
     var undef /*left undefined*/;
     
-    var viewWidth = 200;
-    var viewHeight = 200;
+    var viewWidth = BLOCK_SIZE * 80;
+    var viewHeight = BLOCK_SIZE * 48;
     var renderer = null;
     var stage = null;
 
@@ -30,11 +30,40 @@ function start() {
 
     //var game = new Game();
     //game.setSpeed() -> snake
-
     //game.setBoard(board);
     //game.addSnake(snake):
-
     //var board = new Board(100, 100);
+
+    var levelLoader = ImageLevelLoader.getInstance();
+
+    var wallTexture = new PIXI.Texture.fromImage('resources/images/sprites/wall.png');
+
+    /*
+    */
+    var done = function(data){
+        for(var y = 0; y < data.length; y++){
+            for(var x = 0; x < data[y].length; x++){
+
+                if(data[y][x] === 1){
+                    var sprite = new PIXI.Sprite(wallTexture);
+                    sprite.position.x = x * BLOCK_SIZE;
+                    sprite.position.y = y * BLOCK_SIZE;
+                    stage.addChild(sprite);
+                }
+            }
+        }
+    };
+
+    levelLoader.load({
+        levelPath: 'resources/levels/level1.png',
+        translation: {
+            empty: 0,
+            wall: 1,
+            start: 2
+        },
+        done: done
+     });
+
 
     var snake = new Snake(stage);
     snake.init(30);
