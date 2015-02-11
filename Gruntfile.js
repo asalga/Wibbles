8e2a407d4e67ca4ec55d96c34a6bd7e86051149f
Andor Salga <andorsalga@gmail.com>
2015-02-10 19:55:49 -0500

    adding grunt

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt){
+
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+
+    uglify: {
+      build: {
+        src: 'src/*',
+	dest: 'build/<%= pkg.name %>.min.js'
+      }      
+    }  
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify']);
+
+};
